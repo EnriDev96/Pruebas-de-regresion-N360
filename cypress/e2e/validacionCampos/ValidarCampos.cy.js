9c1af855e5d2fa7aa16355124fcbd98718daee34
EnriDev96 <bryan.orellana.g@gmail.com>
2025-08-15 14:52:56 -0500

    Aislamiento de Validaciones

--- cypress/e2e/validacionCampos/ValidarCampos.cy.js
+++ cypress/e2e/validacionCampos/ValidarCampos.cy.js
@@ -1,44 +1,28 @@
-import FichaPersonalValidation from "../../support/page-objects/fichaPersonalPage/FichaPersonalPageValidation";
-import ContratoPageValidation from "../../support/page-objects/contratosPages/ContratoPageValidation";
+import FichaPersonalValidation from "../../support/page-objects/validaciones/fichaPersonalValidation/FichaPersonalValidation";
 import { validationReporter } from "../../support/utils/validationReporter";
 
 describe("Validaciones de Campos de Formularios", () => {
   const fpValidation = new FichaPersonalValidation();
-  const conValidation = new ContratoPageValidation();
 
   beforeEach(() => {
     cy.loginNomina360();
   });
 
   afterEach(() => {
-    // Reporta y falla si hay errores
     validationReporter.reportAndAssertAll();
     validationReporter.clearErrors();
   });
 
-  it("1 Validar Campos Ficha Personal Ivalido", () => {
+  it.only("1 Validar Campos Obligatorios Ficha Personal", () => {
+    cy.fixture("validationFixtures/fichaPersonal").then((data) => {
+      fpValidation.goToFichaPersonal();
+      fpValidation.validarCamposFichaPersonal(data.empleadoEmpty);
+    });
+  });
+  it("2 Validar Formato de Campos Ficha Personal", () => {
     cy.fixture("fichaPersonal").then((data) => {
       fpValidation.goToFichaPersonal();
       fpValidation.crearFichaPersonal(data.empleadoInvalido);
-    });
-  });
-  it("2 Validar Campos Ficha Personal", () => {
-    cy.fixture("fichaPersonal").then((data) => {
-      fpValidation.goToFichaPersonal();
-      fpValidation.crearFichaPersonal(data.empleadoUno);
-    });
-  });
-  it("3 Validar Campos Contrato Invalido", () => {
-    cy.fixture("contratos").then((dataContrato) => {
-      conValidation.goToContratos();
-      conValidation.crearContrato(dataContrato.contratoDemoInvalido);
-    });
-  });
-
-  it("4 Validar Campos Contrato", () => {
-    cy.fixture("contratos").then((dataContrato) => {
-      conValidation.goToContratos();
-      conValidation.crearContrato(dataContrato.contratoDemo);
     });
   });
 });
